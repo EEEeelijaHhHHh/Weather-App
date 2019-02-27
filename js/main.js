14ea02d5a3c85835dc165bfed0902074f9920b52
EEEeelijaHhhHHh <prostoilja1@gmail.com>
2019-02-27 13:56:12 +0200

    ''

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
+$('document').ready(function () {
+  let widgetSlider = function () {
+    $('.slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      nextArrow: '<a href="#"><img src="img/slider/arrow_next.png" class="arrow-next"></img></a>',
+      prevArrow: '<a href="#"><img src="img/slider/arrow_prev.png" class="arrow-prev"></img></a>',
+      appendArrows: $('.slider__arrows'),
+    });
+  }
 
-  
+  let anchorScroll = function () {
+    $('a').on('click', function () {
+      $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+      return false;
+    });
+  }
 
+  widgetSlider();
+  anchorScroll();
 });
